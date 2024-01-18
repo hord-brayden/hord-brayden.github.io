e7f9b26b5ce65b694f82cab93ab37d2eac61ac40
Brayden H <35241666+hord-brayden@users.noreply.github.com>
2024-01-17 21:32:05 -0700

    Update dark_mode_toggle.js

--- js/dark_mode_toggle.js
+++ js/dark_mode_toggle.js
@@ -13,14 +13,8 @@ const darkModeToggle = document.querySelector('#dark-mode-toggle-btn');
 const darkMode = localStorage.getItem('darkMode');
 const isOsDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-// Set dark mode based on OS preference
-if (isOsDarkMode) {
-    document.body.classList.add('dark-mode');
-    darkModeToggle.checked = true;
-}
-
-// Toggle dark mode based on user preference
-if (darkMode === 'enabled') {
+// Set dark mode based on OS/user preference
+if (isOsDarkMode || darkMode === 'enabled') {
     document.body.classList.add('dark-mode');
     darkModeToggle.checked = true;
 }
