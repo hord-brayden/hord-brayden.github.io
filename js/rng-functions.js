30c9e5144806fa405376c2ed8b8d280eb07619a9
Brayden Anthony H <35241666+hord-brayden@users.noreply.github.com>
2024-05-14 22:19:32 -0600

    Update rng-functions.js
    dumb dumb dumb

--- js/rng-functions.js
+++ js/rng-functions.js
@@ -1,14 +1,14 @@
 import { XORShift, LCG } from './rng-classes.js';
 
-function getRNGFunction(method, seed) {
+function getRNGFunction(method, seed, gameState = null) {
   switch (method) {
     case "mathRandom":
       return () => Math.random();
     case "xorShift":
-      const xorShiftRng = new XORShift(seed);
+      const xorShiftRng = new XORShift(seed, gameState);
       return () => xorShiftRng.random();
     case "lcg":
-      const lcgRng = new LCG(seed);
+      const lcgRng = new LCG(seed, gameState);
       return () => lcgRng.random();
     case "cryptoRandomValues":
       return () => crypto.getRandomValues(new Uint32Array(1))[0] / 4294967296;
